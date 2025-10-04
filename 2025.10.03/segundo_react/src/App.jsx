f9056b263a3e671915dcd742481ae47e6851b98c
Cauan Moronhe <cauanmoronhelopes@gmail.com>
2025-10-03 21:28:52 -0300

    Segundo React Components

---
+++ 2025.10.03/segundo_react/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import pixel from './assets/pixel_art.jpg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Hello World {2+2}</h1>
+      <h2>Data:{Date()}</h2>
+        <a href="https://google.com.br" id="link"> Google </a>
+
+        <ul class="lista">
+          <li>SEGUNDA</li>
+          <li>TERÇA</li>
+          <li>QUARTA</li>
+        </ul>
+
+        <img src='/CrashThereIsFuck.jpg'></img>
+        <img src='/KaibaMacaco.jpg'></img>
+
+        <ol class="lista">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JAVASCRIPT</li>
+        </ol>
+        <img src={pixel}></img>
+    </div>
+  )
+}
+
+export default App
